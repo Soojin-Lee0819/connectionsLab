a5bdde22bfb0a8bec3907247e1b2c9f7f1317b84
Soojin Lee <sl7362@nyu.edu>
2022-03-08 00:59:37 +0400

    push
    push

--- week6_Starbucks_node_express/index.js
+++ week6_Starbucks_node_express/index.js
@@ -4,481 +4,9 @@ let express = require('express');
 const res = require('express/lib/response');
 let app = express();
 
-
-// JSON format data for Beverages in NYUAD Library Cafe
-let coffeeData = {
-  "coffee": 
-    [
-      {
-        "drink_name": "Caffe Latte",
-        "type": "Hot Coffees",
-        "size": "Tall",
-        "calories": 150,
-        "fat": 6,
-        "cholesterol": 25,
-        "sodium": 135,
-        "carb": 15,
-        "sugar": 14,
-        "protein": 10,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "Caffe Latte",
-        "type": "Hot Coffees",
-        "size": "Grande",
-        "calories": 190,
-        "fat": 7,
-        "cholesterol": 30,
-        "sodium": 170,
-        "carb": 19,
-        "sugar": 18,
-        "protein": 13,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Cappuccino",
-        "type": "Hot Coffees",
-        "size": "Tall",
-        "calories": 90,
-        "fat": 3.5,
-        "cholesterol": 15,
-        "sodium": 80,
-        "carb": 9,
-        "sugar": 8,
-        "protein": 6,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "Cappuccino",
-        "type": "Hot Coffees",
-        "size": "Grande",
-        "calories": 120,
-        "fat": 4,
-        "cholesterol": 15,
-        "sodium": 100,
-        "carb": 12,
-        "sugar": 10,
-        "protein": 8,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Caffe Mocha",
-        "type": "Hot Coffees",
-        "size": "Tall",
-        "calories": 290,
-        "fat": 13,
-        "cholesterol": 40,
-        "sodium": 120,
-        "carb": 34,
-        "sugar": 28,
-        "protein": 11,
-        "caffeine": 95
-      },
-      {
-        "drink_name": "Caffe Mocha",
-        "type": "Hot Coffees",
-        "size": "Grande",
-        "calories": 360,
-        "fat": 15,
-        "cholesterol": 50,
-        "sodium": 150,
-        "carb": 44,
-        "sugar": 35,
-        "protein": 13,
-        "caffeine": 175
-      },
-      {
-        "drink_name": "Caramel Macchiato",
-        "type": "Hot Coffees",
-        "size": "Tall",
-        "calories": 190,
-        "fat": 6,
-        "cholesterol": 20,
-        "sodium": 120,
-        "carb": 26,
-        "sugar": 25,
-        "protein": 8,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "Caramel Macchiato",
-        "type": "Hot Coffees",
-        "size": "Grande",
-        "calories": 250,
-        "fat": 7,
-        "cholesterol": 25,
-        "sodium": 150,
-        "carb": 35,
-        "sugar": 33,
-        "protein": 10,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Caffe Americano",
-        "type": "Hot Coffees",
-        "size": "Tall",
-        "calories": 10,
-        "fat": 0,
-        "cholesterol": 0,
-        "sodium": 10,
-        "carb": 1,
-        "sugar": 0,
-        "protein": 1,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Caffe Americano",
-        "type": "Hot Coffees",
-        "size": "Grande",
-        "calories": 15,
-        "fat": 0,
-        "cholesterol": 0,
-        "sodium": 10,
-        "carb": 2,
-        "sugar": 0,
-        "protein": 1,
-        "caffeine": 225
-      },
-      {
-        "drink_name": "White Chocolate Mocha",
-        "type": "Hot Coffees",
-        "size": "Tall",
-        "calories": 340,
-        "fat": 14,
-        "cholesterol": 45,
-        "sodium": 190,
-        "carb": 42,
-        "sugar": 41,
-        "protein": 11,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "White Chocolate Mocha",
-        "type": "Hot Coffees",
-        "size": "Grande",
-        "calories": 430,
-        "fat": 18,
-        "cholesterol": 55,
-        "sodium": 250,
-        "carb": 55,
-        "sugar": 53,
-        "protein": 14,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Espresso",
-        "type": "Hot Coffees",
-        "size": "Solo",
-        "calories": 5,
-        "fat": 0,
-        "cholesterol": 0,
-        "sodium": 0,
-        "carb": 1,
-        "sugar": 0,
-        "protein": 0,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "Espresso",
-        "type": "Hot Coffees",
-        "size": "Doppio",
-        "calories": 10,
-        "fat": 0,
-        "cholesterol": 0,
-        "sodium": 0,
-        "carb": 2,
-        "sugar": 0,
-        "protein": 1,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Hot Chocolate",
-        "type": "Hot Drinks",
-        "size": "Tall",
-        "calories": 270,
-        "fat": 12,
-        "cholesterol": 35,
-        "sodium": 105,
-        "carb": 32,
-        "sugar": 27,
-        "protein": 10,
-        "caffeine": 20
-      },
-      {
-        "drink_name": "Hot Chocolate",
-        "type": "Hot Drinks",
-        "size": "Grande",
-        "calories": 370,
-        "fat": 15,
-        "cholesterol": 45,
-        "sodium": 160,
-        "carb": 44,
-        "sugar": 38,
-        "protein": 14,
-        "caffeine": 25
-      },
-      {
-        "drink_name": "Iced Caffe Latte",
-        "type": "Cold Coffees",
-        "size": "Tall",
-        "calories": 100,
-        "fat": 3.5,
-        "cholesterol": 15,
-        "sodium": 90,
-        "carb": 10,
-        "sugar": 9,
-        "protein": 6,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "Iced Caffe Latte",
-        "type": "Cold Coffees",
-        "size": "Grande",
-        "calories": 130,
-        "fat": 4.5,
-        "cholesterol": 20,
-        "sodium": 115,
-        "carb": 13,
-        "sugar": 11,
-        "protein": 8,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Iced Caffe Americano",
-        "type": "Cold Coffees",
-        "size": "Tall",
-        "calories": 10,
-        "fat": 0,
-        "cholesterol": 0,
-        "sodium": 10,
-        "carb": 1,
-        "sugar": 0,
-        "protein": 1,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Iced Caffe Americano",
-        "type": "Cold Coffees",
-        "size": "Grande",
-        "calories": 15,
-        "fat": 0,
-        "cholesterol": 0,
-        "sodium": 15,
-        "carb": 2,
-        "sugar": 0,
-        "protein": 1,
-        "caffeine": 225
-      },
-      {
-        "drink_name": "Iced Caramel Macchiato",
-        "type": "Cold Coffees",
-        "size": "Tall",
-        "calories": 180,
-        "fat": 5,
-        "cholesterol": 20,
-        "sodium": 110,
-        "carb": 26,
-        "sugar": 24,
-        "protein": 7,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "Iced Caramel Macchiato",
-        "type": "Cold Coffees",
-        "size": "Grande",
-        "calories": 250,
-        "fat": 7,
-        "cholesterol": 25,
-        "sodium": 150,
-        "carb": 37,
-        "sugar": 34,
-        "protein": 10,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Iced Caffe Mocha",
-        "type": "Cold Coffees",
-        "size": "Tall",
-        "calories": 260,
-        "fat": 12,
-        "cholesterol": 40,
-        "sodium": 80,
-        "carb": 29,
-        "sugar": 23,
-        "protein": 7,
-        "caffeine": 95
-      },
-      {
-        "drink_name": "Iced Caffe Mocha",
-        "type": "Cold Coffees",
-        "size": "Grande",
-        "calories": 350,
-        "fat": 17,
-        "cholesterol": 55,
-        "sodium": 100,
-        "carb": 39,
-        "sugar": 30,
-        "protein": 10,
-        "caffeine": 175
-      },
-      {
-        "drink_name": "Iced White Chocolate Mocha",
-        "type": "Cold Coffees",
-        "size": "Tall",
-        "calories": 310,
-        "fat": 14,
-        "cholesterol": 45,
-        "sodium": 150,
-        "carb": 38,
-        "sugar": 37,
-        "protein": 8,
-        "caffeine": 75
-      },
-      {
-        "drink_name": "Iced White Chocolate Mocha",
-        "type": "Cold Coffees",
-        "size": "Grande",
-        "calories": 420,
-        "fat": 20,
-        "cholesterol": 60,
-        "sodium": 200,
-        "carb": 50,
-        "sugar": 49,
-        "protein": 11,
-        "caffeine": 150
-      },
-      {
-        "drink_name": "Espresso Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Tall",
-        "calories": 140,
-        "fat": 1.5,
-        "cholesterol": 5,
-        "sodium": 140,
-        "carb": 29,
-        "sugar": 28,
-        "protein": 2,
-        "caffeine": 125
-      },
-      {
-        "drink_name": "Espresso Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Grande",
-        "calories": 210,
-        "fat": 2.5,
-        "cholesterol": 5,
-        "sodium": 210,
-        "carb": 43,
-        "sugar": 42,
-        "protein": 3,
-        "caffeine": 155
-      },
-      {
-        "drink_name": "Caramel Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Tall",
-        "calories": 260,
-        "fat": 11,
-        "cholesterol": 35,
-        "sodium": 160,
-        "carb": 38,
-        "sugar": 38,
-        "protein": 3,
-        "caffeine": 60
-      },
-      {
-        "drink_name": "Caramel Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Grande",
-        "calories": 370,
-        "fat": 15,
-        "cholesterol": 45,
-        "sodium": 230,
-        "carb": 57,
-        "sugar": 55,
-        "protein": 4,
-        "caffeine": 90
-      },
-      {
-        "drink_name": "Mocha Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Tall",
-        "calories": 240,
-        "fat": 9,
-        "cholesterol": 25,
-        "sodium": 150,
-        "carb": 37,
-        "sugar": 35,
-        "protein": 4,
-        "caffeine": 70
-      },
-      {
-        "drink_name": "Mocha Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Grande",
-        "calories": 370,
-        "fat": 15,
-        "cholesterol": 45,
-        "sodium": 220,
-        "carb": 55,
-        "sugar": 52,
-        "protein": 5,
-        "caffeine": 100
-      },
-      {
-        "drink_name": "Caffè Vanilla Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Tall",
-        "calories": 280,
-        "fat": 10,
-        "cholesterol": 30,
-        "sodium": 160,
-        "carb": 45,
-        "sugar": 44,
-        "protein": 3,
-        "caffeine": 65
-      },
-      {
-        "drink_name": "Caffè Vanilla Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Grande",
-        "calories": 400,
-        "fat": 14,
-        "cholesterol": 45,
-        "sodium": 230,
-        "carb": 65,
-        "sugar": 0,
-        "protein": 4,
-        "caffeine": 95
-      },
-      {
-        "drink_name": "White Chocolate Mocha Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Tall",
-        "calories": 280,
-        "fat": 11,
-        "cholesterol": 30,
-        "sodium": 180,
-        "carb": 41,
-        "sugar": 40,
-        "protein": 4,
-        "caffeine": 65
-      },
-      {
-        "drink_name": "White Chocolate Mocha Frappuccino Blended",
-        "type": "Frappuccino Blended Beverages",
-        "size": "Grande",
-        "calories": 410,
-        "fat": 16,
-        "cholesterol": 45,
-        "sodium": 260,
-        "carb": 63,
-        "sugar": 62,
-        "protein": 5,
-        "caffeine": 95
-      }
-     ]
-};
+//import json
+const coffeeData = require("./coffee.json");
+console.log(coffeeData);
 
 
 app.use('/', express.static('public'));
@@ -492,51 +20,50 @@ app.get('/random', (req, response) => {
 })
 
 //show all coffee JSON
-app.get('/coffee', (req,res) => {
+app.get('/coffee', (req, res) => {
   let maxCal = req.query.calorieLowerThan;
 
-  lowerCalorieDrinks = {};
+  let lowerCalorieDrinks = {};
   // respond with a json of drinks less than a certain calorie
-  for(drink in coffeeData.coffee) // loops through the keys in campusCats 
+  for (drink in coffeeData.coffee) // loops through the keys in campusCats 
   {
     let lowerCalorieDrink = coffeeData.coffee[drink];
-    if(lowerCalorieDrink.calories <= maxCal) {
+    if (lowerCalorieDrink.calories <= maxCal) {
       lowerCalorieDrinks[drink] = lowerCalorieDrink;
     }
   }
-
-// add conditions so that when user asks for coffee with higher calorie, the API should still work
-  if(maxCal) {
+  console.log(lowerCalorieDrinks);
+  // add conditions so that when user asks for coffee with higher calorie, the API should still work
+  if (maxCal) {
     res.json(lowerCalorieDrinks);
   } else {
-    res.json({status: "info not present"});
+    res.json({ status: "info not present" });
   }
 })
 
 
-
-app.get('/coffee/:coffeeName', (req,res)=> {
+app.get('/coffee/:coffeeName', (req, res) => {
   console.log(req.params.coffeeName);
   let user_coffeeName = req.params.coffeeName;
   let user_obj;
-  for(let i=0;i<coffeeData.coffee.length;i++) {
-      if(user_coffeeName == coffeeData.coffee[i].drink_name) {
-          user_obj = coffeeData.coffee[i];
-          
-      }
+  for (let i = 0; i < coffeeData.coffee.length; i++) {
+    if (user_coffeeName == coffeeData.coffee[i].drink_name) {
+      user_obj = coffeeData.coffee[i];
+
+    }
   }
   console.log(user_obj);
-  if(user_obj) {
-      res.json(user_obj);
+  if (user_obj) {
+    res.json(user_obj);
   } else {
-      res.json({status: "this drink does not exist"});
+    res.json({ status: "this drink does not exist" });
   }
 })
 
 
 //what port whould we listen to?
 app.listen(9000, () => {
-    console.log("the server is up and running");
+  console.log("the server is up and running");
 })
 
 
--- week6_Starbucks_node_express/public/app.js
+++ week6_Starbucks_node_express/public/app.js
@@ -1,13 +1,12 @@
 console.log("this is the browser");
 
 
+//Request random coffee
+let coffeeButton = document.getElementById("coffee-button");
+coffeeButton.addEventListener("click", () => {
 
-    //Request random coffee
-    let coffeeButton = document.getElementById("coffee-button");
-    coffeeButton.addEventListener("click", () => {
-
-        //Request random coffee object
-        fetch("/random")
+    //Request random coffee object
+    fetch("/random")
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -21,7 +20,6 @@ console.log("this is the browser");
             let drinksugar = data.sugar;
             let drinkprotein = data.protein;
             let drinkcaffeine = data.caffeine;
-            console.log (drinkcarb);
 
             document.getElementById("coffee-random").innerHTML = drinkname;
 
@@ -50,7 +48,212 @@ console.log("this is the browser");
             <p> ${drinkcaffeine} mg</p>
                `
         });
-    });
+});
 
 
 
+//Request low calorie random coffee
+let coffeeButtonlow = document.getElementById("coffee-button-low");
+coffeeButtonlow.addEventListener("click", () => {
+    //Request query object: only coffee that is calorie lower than 100
+    fetch("/coffee?calorieLowerThan=100")
+        .then(response => response.json())
+        .then(data => {
+            //push fetched data (lowercaloriedrinks) to newlowCalorieArray
+            let newlowCalorieArray = [];
+            for (drink in data) {
+                newlowCalorieArray.push(data[drink]);
+            }
+            let randomNum = Math.floor(Math.random() * newlowCalorieArray.length);
+            console.log(newlowCalorieArray[randomNum])
+            let thisdrink = newlowCalorieArray[randomNum]
+            let drinkname = thisdrink.drink_name;
+            let drinksize = thisdrink.size;
+            let drinkcalories = thisdrink.calories;
+            let drinkfat = thisdrink.fat;
+            let drinkcholesterol = thisdrink.cholesterol;
+            let drinksodium = thisdrink.sodium;
+            let drinkcarb = thisdrink.carb;
+            let drinksugar = thisdrink.sugar;
+            let drinkprotein = thisdrink.protein;
+            let drinkcaffeine = thisdrink.caffeine;
+
+            document.getElementById("coffee-random").innerHTML = drinkname;
+
+            document.getElementById("drink_info_left").innerHTML = `
+
+        <p> size </p>
+        <p> calories </p>
+        <p> fat </p>
+        <p> cholesterol </p>
+        <p> sodium </p>
+        <p> carb </p>
+        <p> sugar </p>
+        <p> protein </p>
+        <p> caffeine </p>
+
+        `
+            document.getElementById("drink_info_right").innerHTML = `
+        <p> ${drinksize}</p>
+        <p> ${drinkcalories} kcal </p>
+        <p> ${drinkfat} g</p>
+        <p> ${drinkcholesterol} mg</p>
+        <p> ${drinksodium} mg</p>
+        <p> ${drinkcarb} g</p>
+        <p> ${drinksugar} g</p>
+        <p> ${drinkprotein} g</p>
+        <p> ${drinkcaffeine} mg</p>
+           `
+
+        });
+});
+
+
+//Request specific coffee
+
+//Request Americano
+let americanoButton = document.getElementById("americano");
+americanoButton.addEventListener("click", () => {
+        //Request params:Iced Caffe Americano
+        fetch('/coffee/Iced Caffe Americano')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let drinkname = data.drink_name;
+            let drinksize = data.size;
+            let drinkcalories = data.calories;
+            let drinkfat = data.fat;
+            let drinkcholesterol = data.cholesterol;
+            let drinksodium = data.sodium;
+            let drinkcarb = data.carb;
+            let drinksugar = data.sugar;
+            let drinkprotein = data.protein;
+            let drinkcaffeine = data.caffeine;
+
+            document.getElementById("coffee-random").innerHTML = drinkname;
+
+            document.getElementById("drink_info_left").innerHTML = `
+
+            <p> size </p>
+            <p> calories </p>
+            <p> fat </p>
+            <p> cholesterol </p>
+            <p> sodium </p>
+            <p> carb </p>
+            <p> sugar </p>
+            <p> protein </p>
+            <p> caffeine </p>
+
+            `
+            document.getElementById("drink_info_right").innerHTML = `
+            <p> ${drinksize}</p>
+            <p> ${drinkcalories} kcal </p>
+            <p> ${drinkfat} g</p>
+            <p> ${drinkcholesterol} mg</p>
+            <p> ${drinksodium} mg</p>
+            <p> ${drinkcarb} g</p>
+            <p> ${drinksugar} g</p>
+            <p> ${drinkprotein} g</p>
+            <p> ${drinkcaffeine} mg</p>
+               `
+        })
+
+})
+
+//Request Iced-Cafe-Mocha
+let cafemochaButton = document.getElementById("caffee-mocha");
+cafemochaButton.addEventListener("click", () => {
+        //Request params:Iced-Cafe-Mocha
+        fetch('/coffee/Iced Caffe Mocha')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let drinkname = data.drink_name;
+            let drinksize = data.size;
+            let drinkcalories = data.calories;
+            let drinkfat = data.fat;
+            let drinkcholesterol = data.cholesterol;
+            let drinksodium = data.sodium;
+            let drinkcarb = data.carb;
+            let drinksugar = data.sugar;
+            let drinkprotein = data.protein;
+            let drinkcaffeine = data.caffeine;
+
+            document.getElementById("coffee-random").innerHTML = drinkname;
+
+            document.getElementById("drink_info_left").innerHTML = `
+
+            <p> size </p>
+            <p> calories </p>
+            <p> fat </p>
+            <p> cholesterol </p>
+            <p> sodium </p>
+            <p> carb </p>
+            <p> sugar </p>
+            <p> protein </p>
+            <p> caffeine </p>
+
+            `
+            document.getElementById("drink_info_right").innerHTML = `
+            <p> ${drinksize}</p>
+            <p> ${drinkcalories} kcal </p>
+            <p> ${drinkfat} g</p>
+            <p> ${drinkcholesterol} mg</p>
+            <p> ${drinksodium} mg</p>
+            <p> ${drinkcarb} g</p>
+            <p> ${drinksugar} g</p>
+            <p> ${drinkprotein} g</p>
+            <p> ${drinkcaffeine} mg</p>
+               `
+        })
+
+})
+
+//Request Hot-Chocolate
+let hotChocolateButton = document.getElementById("hot-chocolate");
+hotChocolateButton.addEventListener("click", () => {
+        //Request params:Hot Chocolate
+        fetch('/coffee/Hot Chocolate')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let drinkname = data.drink_name;
+            let drinksize = data.size;
+            let drinkcalories = data.calories;
+            let drinkfat = data.fat;
+            let drinkcholesterol = data.cholesterol;
+            let drinksodium = data.sodium;
+            let drinkcarb = data.carb;
+            let drinksugar = data.sugar;
+            let drinkprotein = data.protein;
+            let drinkcaffeine = data.caffeine;
+
+            document.getElementById("coffee-random").innerHTML = drinkname;
+
+            document.getElementById("drink_info_left").innerHTML = `
+
+            <p> size </p>
+            <p> calories </p>
+            <p> fat </p>
+            <p> cholesterol </p>
+            <p> sodium </p>
+            <p> carb </p>
+            <p> sugar </p>
+            <p> protein </p>
+            <p> caffeine </p>
+
+            `
+            document.getElementById("drink_info_right").innerHTML = `
+            <p> ${drinksize}</p>
+            <p> ${drinkcalories} kcal </p>
+            <p> ${drinkfat} g</p>
+            <p> ${drinkcholesterol} mg</p>
+            <p> ${drinksodium} mg</p>
+            <p> ${drinkcarb} g</p>
+            <p> ${drinksugar} g</p>
+            <p> ${drinkprotein} g</p>
+            <p> ${drinkcaffeine} mg</p>
+               `
+        })
+
+})
