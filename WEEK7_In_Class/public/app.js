7726b805a744d46d35f7c71c20b633a9320f481b
Soojin Lee <sl7362@nyu.edu>
2022-03-14 23:49:31 +0400

    make changes

--- WEEK7_In_Class/public/app.js
+++ WEEK7_In_Class/public/app.js
@@ -4,21 +4,20 @@ window.addEventListener("load",()=> {
 
 //fetch the /messages api and load the information. Show it on the feed
 
+//hide the pop on start
 
-
-
-    //hide the pop on start
-    let createPostDiv = document.getElementById("create_container");
-    createPostDiv.style.display ="none";
+//hide the pop on start
+let createPostDiv = document.getElementById("create_container");
+createPostDiv.style.display ="none";
 
 //BUTTON TO DISPLAY CREATE NEW POST
 let newPostButton = document.getElementById("new_button");
 newPostButton.addEventListener("click",() => {
     console.log("Button CLicked");
-    if(createPostDiv.style.display =="flex"){
+    if(createPostDiv.style.display =="inline"){
         createPostDiv.style.display = "none";
     } else{
-        createPostDiv.style.display ="flex";
+        createPostDiv.style.display ="inline";
     }
 
 })
