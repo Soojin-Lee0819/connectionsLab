bae8e2f02ede645b9f1f2151e307d30ce61e03c7
Soojin Lee <sl7362@nyu.edu>
2022-03-25 19:02:06 +0400

    push
    push

--- week8_In_Class/index.js
+++ week8_In_Class/index.js
@@ -1,34 +1,91 @@
-console.log("this is index.js")
-
+//Initialize the express 'app' object
 let express = require('express');
-let http = require('http');
-let io = require('socket.io');
-
-
 let app = express();
-let server = http.createServer(app); // wrap the express app with http
-io = new io.Server(server) // use socket.io on the http app
-
-
-
 app.use('/', express.static('public'));
 
+//Initialize the actual HTTP server
+let http = require('http');
+let server = http.createServer(app);
+let port = process.env.PORT || 9000;
+server.listen(port, () => {
+    console.log("Server listening at port: " + port);
+});
 
-//when a socket connects, take the socket 
-io.sockets.on('connection',(socket)=> {
-    console.log("we have", socket.id);
-    socket.on('disconnect',()=> {
-        console.log('sockethas been disconnected',socket.id);
-    })
+//Initialize socket.io
+let io = require('socket.io');
+io = new io.Server(server);
+let arrayofClients = [];
+
+//connect
+io.sockets.on('connection', function(socket) {
+
+    //Listen for this client to connect
+    console.log("We have a new client: " + socket.id);
+    //to keep on track of clients 
+    arrayofClients.push(socket.id);
+    console.log(arrayofClients);
+
+    //Listen for this client to disconnect
+    socket.on('disconnect', function() {
+        //when client disconnect, remove client from array
+        arrayofClients.pop(socket.id);
+        console.log("A client has disconnected: " + socket.id);
+    });
 
 //listen for a message from a client
     socket.on('mousePositionData',(data)=>{
         console.log(data);
         io.sockets.emit('mouseDataFromServer', data);
     })
-})
 
-//server listening on port
-server.listen(9000,() => {
-    console.log("sever is up and running")
+
+    //Listen for a message named 'msg' from this client
+    socket.on('msg', function(data) {
+        //Data can be numbers, strings, objects
+        console.log("Received a 'msg' event");
+        console.log(data);
+
+        //Send a response to all clients, including this one
+        io.sockets.emit('msg', data);
+
+        //Send a response to all other clients, not including this one
+        // socket.broadcast.emit('msg', data);
+
+        //Send a response to just this client
+        // socket.emit('msg', data);
+    });
+
+    //Listen for a message named 'randomword' from this client
+    socket.on('randomword', function(data) {
+        //Data can be numbers, strings, objects
+        console.log("Received a 'randomword' event");
+        console.log(data);
+
+        //Send a response to all other clients, not including this one
+        socket.broadcast.emit('randomword', data);
+
+    });
+
+        //Listen for a message named 'displayrandomword' from this client
+        socket.on('displayrandomword', function(data) {
+            //Data can be numbers, strings, objects
+            console.log("Received a 'displayrandomword' event");
+            console.log(data);
+    
+            //Send a response to just this client
+            socket.emit('displayrandomword', data);
+    
+        });
+
+
+            //Listen for a message named 'matchingword' 
+            socket.on('matchingword', function(data) {
+          
+                 //Send a response to all cients 
+                io.sockets.emit('matchingword', data);
+        
+            });
+
+
 });
+
